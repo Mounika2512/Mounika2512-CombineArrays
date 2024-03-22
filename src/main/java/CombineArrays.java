70ae1ad8e957058e28d385bbb8160a2852deb341
Mounika2512 <mounikasaviti25@gmail.com>
2024-03-22 11:05:16 +0530

    combinearrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,18 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+ int totalLength = arr1.length + arr2.length;
 
-        return null;
+        // Create a new array to store the combined elements
+        int[] combinedArray = new int[totalLength];
+
+        // Copy elements from arr1 to the combined array
+        System.arraycopy(arr1, 0, combinedArray, 0, arr1.length);
+
+        // Copy elements from arr2 starting from the end of arr1 in the combined array
+        System.arraycopy(arr2, 0, combinedArray, arr1.length, arr2.length);
+
+        // Return the combined array
+        return combinedArray;
     }
 }
